46c5bf0e3eb5092d3f79a25c7de3e3f2559c83ad
Evans <69148336+evans-0@users.noreply.github.com>
2025-08-18 17:20:02 +0530

    Add files via upload

---
+++ C/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "matrix.h"
+
+int main(){
+	Matrix M0 = init(3,3);
+	create(&M0);
+	view(M0);
+	float f;
+	det(&f, M0);
+	printf("determinant -> %f\n", f);
+	Matrix M1 = init(3,3);
+	//matcpy(&M1, M0);	// Copying M0 to M1
+	create(&M1);
+	mul(&M1, M0);	// Multiply M0 by M1 and store in M1
+	view(M1);
+
+	return 0;
+}
